ae43cd8b5269f78d640b392bb9af12efaec980d6
qinmingjie <1004537433@qq.com>
2021-09-08 01:44:29 +0800

    refactor: 调整全局变量

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@
  * @Author: mjqin
  * @Date: 2021-05-18 23:36:07
  * @LastEditors: mjqin
- * @LastEditTime: 2021-09-07 00:08:52
+ * @LastEditTime: 2021-09-08 01:43:42
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 import route from './router'
 import ElementPlus from 'element-plus'
+import { ElMessage } from 'element-plus' 
 import 'element-plus/lib/theme-chalk/index.css';
 import request from './utils/request'
 import storage from './utils/storage'
@@ -21,6 +22,9 @@ console.log("环境变量=>", import.meta.env)
 const app = createApp(App);
 app.config.globalProperties.$request = request;
 app.config.globalProperties.$api = api;
-app.config.globalProperties.aaaaa = 'aaaaa';
 app.config.globalProperties.$storage = storage;
+app.provide('$api', api);
+app.provide('$message', ElMessage)
+app.provide('$request', request)
+app.provide('$storage', storage)
 app.use(route).use(store).use(ElementPlus, { size: 'small' }).mount('#app')
--- src/pages/user-manage/ConsumerPage.vue
+++ src/pages/user-manage/ConsumerPage.vue
@@ -40,84 +40,69 @@
     </el-card>
   </div>
 </template>
-<script>
-import { getCurrentInstance, onMounted, reactive, ref } from "vue"
+<script setup>
+import { getCurrentInstance, inject, onMounted, reactive, ref } from "vue"
 import utils from "@/utils/utils"
 import { ElMessage } from "element-plus"
-export default {
-  name: "ConsumerPage",
-  setup() {
-    const { ctx } = getCurrentInstance()
-    const userList = ref([])
-    const pager = reactive({
-      page: 1,
-      limit: 10,
-      total: 10,
-    })
-    const formData = reactive({
-      keyword: "",
-    })
-    const isTableLoading = ref(false)
-    // 定义动态表格-格式
-    const columns = reactive([
-      {
-        label: "用户ID",
-        prop: "id",
-        minWidth: 100,
-      },
-      {
-        label: "用户名",
-        prop: "name",
-        minWidth: 120,
-      },
-      {
-        label: "注册时间",
-        prop: "createdAt",
-        width: 180,
-        formatter: (row, column, value) => {
-          return utils.formateDate(new Date(value))
-        },
-      },
-    ])
-    // 初始化接口调用
-    onMounted(() => {
-      getUserList()
-    })
-    // 获取用户列表
-    const getUserList = async () => {
-      isTableLoading.value = true
-      const params = { keyword: formData.keyword, ...pager }
-      try {
-        const { count, rows } = await ctx.$api.getUserList(params)
-        userList.value = rows
-        pager.total = count
-      } catch (error) {
-        console.log(error)
-        ElMessage.error(error.msg || error)
-      }
-      isTableLoading.value = false
-    }
-    // 分页事件处理
-    const handleCurrentChange = (current) => {
-      pager.page = current
-      getUserList()
-    }
-
-    const handleDetail = (row) => {
-      return row
-    }
-
-    return {
-      isTableLoading,
-      formData,
-      userList,
-      columns,
-      pager,
-      getUserList,
-      handleCurrentChange,
-      handleDetail,
-    }
+const { ctx } = getCurrentInstance()
+const $api = inject("$api")
+const userList = ref([])
+const pager = reactive({
+  page: 1,
+  limit: 10,
+  total: 10,
+})
+const formData = reactive({
+  keyword: "",
+})
+const isTableLoading = ref(false)
+// 定义动态表格-格式
+const columns = reactive([
+  {
+    label: "用户ID",
+    prop: "id",
+    minWidth: 100,
   },
+  {
+    label: "用户名",
+    prop: "name",
+    minWidth: 120,
+  },
+  {
+    label: "注册时间",
+    prop: "createdAt",
+    width: 180,
+    formatter: (row, column, value) => {
+      return utils.formateDate(new Date(value))
+    },
+  },
+])
+// 初始化接口调用
+onMounted(() => {
+  getUserList()
+})
+// 获取用户列表
+const getUserList = async () => {
+  isTableLoading.value = true
+  const params = { keyword: formData.keyword, ...pager }
+  try {
+    const { count, rows } = await $api.getUserList(params)
+    userList.value = rows
+    pager.total = count
+  } catch (error) {
+    console.log(error)
+    ElMessage.error(error.msg || error)
+  }
+  isTableLoading.value = false
+}
+// 分页事件处理
+const handleCurrentChange = (current) => {
+  pager.page = current
+  getUserList()
+}
+
+const handleDetail = (row) => {
+  return row
 }
 </script>
 
